6f91d7782f79545c887e14d779a2e53028f75191
Victorio Iglesias Gutierrez <victorio.iglesias@the-cocktail.com>
2022-04-03 10:01:58 +0200

    Try changing heroku log

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}:3000',
+          url: 'https://{defaultHost}',
           variables: {
             defaultHost: {
               default: 'stores-api-pj.herokuapp.com'
